3a007851b4ef4d0f62952a92aa3696a5b66d286c
Yorber Vivas <yorber0104@gmail.com>
2024-03-06 20:42:27 -0400

    add files

--- hack_10.py
+++ hack_10.py
@@ -3,6 +3,17 @@ text: "fooziman" output => ["F","0","0","Z","1","M","@","N"]
 """
 
 def fn_hack_10():
-    result = "fooziman"
-    #...
-    return result  
+    result = list("fooziman")
+    new_result = ["F","0","0","Z","1","M","@","N"]
+
+    for i in range(len(result)):
+        result[i] = new_result[i]
+    print(result)
+    
+    return result
+
+
+fn_hack_10()
+
+
+
